1227defe797c71aa8a54f87afd2c9488d1b32b9f
Bershadsky Mihail Sergeevich <Michail.Bershadsky@ilimtimber.corp>
2021-09-14 16:51:53 +0800

    Starting js, intro tabs done

--- dist/script.js
+++ dist/script.js
@@ -94,9 +94,32 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
+ // INTRO TABS
 
+const tabLinks = document.querySelectorAll('.intro__tablink'),
+      tabContent = document.querySelectorAll('.intro__tab-content > div');
+tabLinks.forEach((link, num, arr) => {
+  link.addEventListener('click', () => {
+    removeActive(arr, 'intro__tablink_active');
+    removeActive(tabContent, 'intro__tab-content_active');
+    addActive(link);
+    chooseContent(num, tabContent);
+  });
+});
 
-console.log('Hello');
+const removeActive = (elems, activeClass) => {
+  elems.forEach(item => {
+    item.classList.remove(activeClass);
+  });
+};
+
+const addActive = elem => {
+  elem.classList.add('intro__tablink_active');
+};
+
+const chooseContent = (num, tabContentElems) => {
+  tabContentElems[num].classList.add('intro__tab-content_active');
+}; // INTRO TABS END
 
 /***/ })
 
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,2 +1,30 @@
 'use strict';
-console.log('Hello');
+
+// INTRO TABS
+
+const tabLinks = document.querySelectorAll('.intro__tablink'),
+      tabContent = document.querySelectorAll('.intro__tab-content > div');
+tabLinks.forEach((link, num, arr) => {
+    link.addEventListener('click', () => {
+        removeActive(arr, 'intro__tablink_active');
+        removeActive(tabContent, 'intro__tab-content_active');
+        addActive(link);
+        chooseContent(num, tabContent);
+    })
+})
+
+const removeActive = (elems, activeClass) => {
+    elems.forEach(item => {
+        item.classList.remove(activeClass);
+    })
+}
+
+const addActive = (elem) => {
+    elem.classList.add('intro__tablink_active');
+}
+
+const chooseContent = (num, tabContentElems) => {
+    tabContentElems[num].classList.add('intro__tab-content_active');
+}
+
+// INTRO TABS END
