e3818346a482cce38ad514e5e979c5ede73f3678
Bershadsky Mihail Sergeevich <Michail.Bershadsky@ilimtimber.corp>
2021-09-15 11:11:41 +0800

    slider done, news tabs done, links done, pureEye need to do

--- dist/script.js
+++ dist/script.js
@@ -94,32 +94,53 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
- // INTRO TABS
 
-const tabLinks = document.querySelectorAll('.intro__tablink'),
-      tabContent = document.querySelectorAll('.intro__tab-content > div');
-tabLinks.forEach((link, num, arr) => {
-  link.addEventListener('click', () => {
-    removeActive(arr, 'intro__tablink_active');
-    removeActive(tabContent, 'intro__tab-content_active');
-    addActive(link);
-    chooseContent(num, tabContent);
-  });
+
+window.addEventListener('DOMContentLoaded', () => {
+  // INTRO TABS
+  function tabs(linkSelector, contentSelector, linkActiveClass, contentActiveClass) {
+    const tabLinks = document.querySelectorAll(linkSelector),
+          tabContent = document.querySelectorAll(contentSelector);
+    tabLinks.forEach((link, num, arr) => {
+      link.addEventListener('click', () => {
+        removeActive(arr, linkActiveClass);
+        removeActive(tabContent, contentActiveClass);
+        addActive(link);
+        chooseContent(num, tabContent);
+      });
+    });
+
+    const removeActive = (elems, activeClass) => {
+      elems.forEach(item => {
+        item.classList.remove(activeClass);
+      });
+    };
+
+    const addActive = elem => {
+      elem.classList.add(linkActiveClass);
+    };
+
+    const chooseContent = (num, tabContentElems) => {
+      tabContentElems[num].classList.add(contentActiveClass);
+    };
+  }
+
+  tabs('.intro__tablink', '.intro__tab-content > div', 'intro__tablink_active', 'intro__tab-content_active'); // INTRO TABS END
+  // INTRO CAROUSEL
+
+  const swiper = new Swiper('.intro__swiper', {
+    loop: true,
+    speed: 400,
+    spaceBetween: 100,
+    navigation: {
+      nextEl: '.intro__swiper-button_next',
+      prevEl: '.intro__swiper-button_prev'
+    }
+  }); // INTRO CAROUSEL END
+  // NEWS TABS
+
+  tabs('.news__tab-item', '.news__tab-content', 'news__tab-item_active', 'news__tab-content_active'); // NEWS TABS END
 });
-
-const removeActive = (elems, activeClass) => {
-  elems.forEach(item => {
-    item.classList.remove(activeClass);
-  });
-};
-
-const addActive = elem => {
-  elem.classList.add('intro__tablink_active');
-};
-
-const chooseContent = (num, tabContentElems) => {
-  tabContentElems[num].classList.add('intro__tab-content_active');
-}; // INTRO TABS END
 
 /***/ })
 
